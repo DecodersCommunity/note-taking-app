05a7669fc66b9d4faaa8d287b262c6109f59b3bc
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2023-10-09 12:14:40 +0530

    it works

--- script.js
+++ script.js
@@ -4,31 +4,32 @@ document.addEventListener("DOMContentLoaded", function () {
     const descriptionEl = document.querySelector('textarea');
     const popupBox = document.querySelector('.popup-box');
     const noteList = document.querySelector('.list-container');
-    const deleteIcon = note.querySelector('#delete');
 
-    // Here is function to add a new note.
+    // Function to add a new note.
     function addNote() {
         const title = titleEl.value.trim();
         const description = descriptionEl.value.trim();
 
         if (title === "") {
-            alert("please write something");
+            alert("Please write something");
         }
+
         if (title !== "") {
             const note = document.createElement('li');
             note.innerHTML = `
-                        <div class="note">
-                            <p>${title}</p>
-                            <span>${description}</span>
-                        </div>
-                        <div class="bottom-content">
-                            <span>Date: ${new Date().toLocaleDateString()}</span>
-                            <div class="settings">
-                                <i class="uil uil-pen" id="edit" onclick="editNote(this)"></i>
-                                <i class="uil uil-trash-alt" id="delete" onclick="removeNote(this)"></i>
-                            </div>
-                        </div>
-                    `;
+                <div class="note">
+                    <p>${title}</p>
+                    <span>${description}</span>
+                </div>
+                <div class="bottom-content">
+                    <span>Date: ${new Date().toLocaleDateString()}</span>
+                    <div class="settings">
+                        <i class="uil uil-pen" id="edit" onclick="editNote(this)"></i>
+                        <i class="uil uil-trash-alt" id="delete" onclick="removeNote(this)"></i>
+                    </div>
+                </div>
+            `;
+
             noteList.appendChild(note);
             titleEl.value = "";
             descriptionEl.value = "";
@@ -44,23 +45,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const addButton = document.querySelector('.content form button');
     addButton.addEventListener('click', addNote);
+
+    trash.addEventListener('click', function (e) {
+        console.log(e);
+    });
+
+    // Function to close the popup.
+    function closePopup() {
+        const popupBox = document.querySelector('.popup-box');
+        popupBox.classList.remove('show');
+    }
+
+    // Event listener for the close button.
+    const closeButton = document.querySelector('.uil-times');
+    closeButton.addEventListener('click', closePopup);
+
+    // Define the removeNote and editNote functions.
+   
+
+    function editNote(element) {
+        // Add your edit functionality here
+    }
 });
 
-// Function to close the popup .
-function closePopup() {
-    const popupBox = document.querySelector('.popup-box');
-    popupBox.classList.remove('show');
-}
-
-// Event listener for the close button .
-const closeButton = document.querySelector('.uil-times');
-closeButton.addEventListener('click', closePopup);
-
-//to remove notes
-function removeNote(iconElement) {
-    const noteElement = iconElement.closest('li');
-
-    if (noteElement) {
-        noteElement.remove();
+function removeNote(element) {
+    const note = element.closest('li');
+    if (note) {
+        note.remove();
     }
 }
