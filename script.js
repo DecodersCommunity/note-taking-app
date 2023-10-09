58660895663aedc75cce762e791dbe9cbec6e414
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2023-10-09 10:10:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const descriptionEl = document.querySelector('textarea');
     const popupBox = document.querySelector('.popup-box');
     const noteList = document.querySelector('.list-container');
+    const deleteIcon = note.querySelector('#delete');
 
     // Here is function to add a new note.
     function addNote() {
@@ -12,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (title === "") {
             alert("please write something");
-            return;
         }
         if (title !== "") {
             const note = document.createElement('li');
@@ -29,64 +29,6 @@ document.addEventListener("DOMContentLoaded", function () {
                             </div>
                         </div>
                     `;
-            document.addEventListener("DOMContentLoaded", function () {
-                const addBox = document.querySelector('.add-box');
-                const titleEl = document.querySelector('input');
-                const descriptionEl = document.querySelector('textarea');
-                const popupBox = document.querySelector('.popup-box');
-                const noteList = document.querySelector('.list-container');
-
-                // Here is function to add a new note.
-                function addNote() {
-                    const title = titleEl.value.trim();
-                    const description = descriptionEl.value.trim();
-
-                    if (title === "") {
-                        alert("please write something");
-                    } else { }
-                    if (title !== "") {
-                        const note = document.createElement('li');
-                        note.innerHTML = `
-                        <div class="note">
-                            <p>${title}</p>
-                            <span>${description}</span>
-                        </div>
-                        <div class="bottom-content">
-                            <span>Date: ${new Date().toLocaleDateString()}</span>
-                            <div class="settings">
-                                <i class="uil uil-pen" id="edit" onclick="editNote(this)"></i>
-                                <i class="uil uil-trash-alt" id="delete" onclick="removeNote(this)"></i>
-                            </div>
-                        </div>
-                    `;
-
-                        noteList.appendChild(note);
-                        titleEl.value = "";
-                        descriptionEl.value = "";
-                        popupBox.classList.remove('show');
-                    }
-                }
-
-                // Event listeners
-                addBox.addEventListener('click', () => {
-                    titleEl.focus();
-                    popupBox.classList.add('show');
-                });
-
-                const addButton = document.querySelector('.content form button');
-                addButton.addEventListener('click', addNote);
-            });
-
-            // Function to close the popup .
-            function closePopup() {
-                const popupBox = document.querySelector('.popup-box');
-                popupBox.classList.remove('show');
-            }
-
-            // Event listener for the close button .
-            const closeButton = document.querySelector('.uil-times');
-            closeButton.addEventListener('click', closePopup);
-
             noteList.appendChild(note);
             titleEl.value = "";
             descriptionEl.value = "";
