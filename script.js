7a6346f2f66d9883643ef40cbc5e28c680950cfe
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2023-10-09 19:31:53 +0530

    made final

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 <div class="bottom-content">
                     <span>Date: ${new Date().toLocaleDateString()}</span>
                     <div class="settings">
-                        <i class="uil uil-pen" id="edit" onclick="editNote(this)"></i>
+                        <i class="uil uil-square-full" id="check" onclick="tickNote(this)"></i>
                         <i class="uil uil-trash-alt" id="delete" onclick="removeNote(this)"></i>
                     </div>
                 </div>
@@ -50,31 +50,27 @@ document.addEventListener("DOMContentLoaded", function () {
     const addButton = document.querySelector('.content form button');
     addButton.addEventListener('click', addNote);
 
-    trash.addEventListener('click', function (e) {
-        console.log(e);
-    });
-
-    // Function to close the popup.
-    function closePopup() {
-        const popupBox = document.querySelector('.popup-box');
-        popupBox.classList.remove('show');
-    }
-
-    // Event listener for the close button.
-    const closeButton = document.querySelector('.uil-times');
-    closeButton.addEventListener('click', closePopup);
-
-    // Define the removeNote and editNote functions.
-   
-
-    function editNote(element) {
-        // Add your edit functionality here
-    }
 });
+
+// Function to close the popup.
+function closePopup() {
+    const popupBox = document.querySelector('.popup-box');
+    popupBox.classList.remove('show');
+}
+
+// Event listener for the close button.
+const closeButton = document.querySelector('.uil-times');
+closeButton.addEventListener('click', closePopup);
+
 
 function removeNote(element) {
     const note = element.closest('li');
     if (note) {
         note.remove();
     }
+}
+
+function tickNote(element) {
+    element.classList.toggle("uil-square-full");
+    element.classList.toggle("uil-check-square");
 }
